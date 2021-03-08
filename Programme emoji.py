6e07743e0ea06cbc41785eb7bd515992e9376fbc
nsi-lfit-sb <79119704+nsi-lfit-sb@users.noreply.github.com>
2021-03-09 08:30:52 +0900

    Update Programme emoji.py

--- Programme emoji.py
+++ Programme emoji.py
@@ -1,11 +1,11 @@
-﻿dic = {"princesse":"\U0001F478"}
-origine = ["princesse", "am", "genius"]
-phrase = []
-for i in range(len(origine)):
-  phrase.append(0)
-  n = origine[i]
-  if n in dic:
-    phrase[i] = dic[n]
+﻿dic = {"princesse":"\U0001F478"} # creation d'un dictionnaire qui associe un mot a un emoji 
+origine = ["princesse", "am", "genius"] # chaque mot de la phrase est place dans une liste 
+phrase = [] # creation d'une liste vide ou l'on va placer les mots remplaces
+for i in range(len(origine)): # pour le nombre de mots dans notre phrase, 
+  phrase.append(0) 
+  n = origine[i] # l'indice n prend le mot d'indice i dans la phrase d'origine
+  if n in dic: # si le mot d'indice n se trouve dans le dictionnaire, 
+    phrase[i] = dic[n] # on remplace notre mot avec l'emoji correspondant 
   else:
-    phrase[i] = origine[i]
-  print(phrase[i],end=' ')
+    phrase[i] = origine[i] # si on ne trouve pas d'emoji, on laisse le mot tel qu'il est 
+  print(phrase[i],end=' ') # affichage de notre phrase finale 
